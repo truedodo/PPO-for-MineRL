7fa595a6d4c0d13787188388c024d5ceb7b033f8
Jack Sullivan <me@jackhsullivan.com>
2023-03-24 22:23:09 -0400

    PPO werks now

--- src/ppo.py
+++ src/ppo.py
@@ -5,24 +5,25 @@ from typing import List
 import gym
 import torch as th
 from torch.utils.data import DataLoader
-
+import matplotlib.pyplot as plt
 import numpy as np
 
 from datetime import datetime
-
 from tqdm import tqdm
-
 from rewards import RewardsCalculator
 from memory import Memory, MemoryDataset
-from util import to_torch_tensor
+from util import to_torch_tensor, normalize
 
 sys.path.insert(0, "vpt")  # nopep8
 
 from agent import MineRLAgent  # nopep8
 from lib.tree_util import tree_map  # nopep8
 
-
+# This is for debugging - Jack
+th.autograd.set_detect_anomaly(True)
 device = th.device("cuda:0" if th.cuda.is_available() else "cpu")
+
+figure, ax = plt.subplots(figsize=(10, 8))
 
 
 class ProximalPolicyOptimizer:
@@ -41,6 +42,10 @@ class ProximalPolicyOptimizer:
             epochs: int = 8,
             minibatch_size: int = 10,
             lr: float = 1e-4,
+            betas: tuple = (0.9, 0.999),
+            beta_s: float = 0.01,
+            eps_clip: float = 0.2,
+            value_clip: float = 0.4,
 
     ):
         self.env_name = env_name
@@ -61,6 +66,10 @@ class ProximalPolicyOptimizer:
         self.epochs = epochs
         self.minibatch_size = minibatch_size
         self.lr = lr
+        self.betas = betas
+        self.beta_s = beta_s
+        self.eps_clip = eps_clip
+        self.value_clip = value_clip
 
         # Load the agent parameters from the weight files
         agent_parameters = pickle.load(open(model_path, "rb"))
@@ -71,6 +80,12 @@ class ProximalPolicyOptimizer:
         self.agent = MineRLAgent(self.env, policy_kwargs=policy_kwargs,
                                  pi_head_kwargs=pi_head_kwargs)
         self.agent.load_weights(weights_path)
+
+        self.optim_pi = th.optim.Adam(
+            self.agent.policy.pi_head.parameters(), lr=lr, betas=betas)
+
+        self.optim_v = th.optim.Adam(
+            self.agent.policy.value_head.parameters(), lr=lr, betas=betas)
 
         # Internal buffer of the most recent episode memories
         # This will be a relatively large chunk of data
@@ -116,11 +131,13 @@ class ProximalPolicyOptimizer:
             agent_obs = tree_map(lambda x: x.unsqueeze(1), agent_obs)
             first = dummy_first.unsqueeze(1)
 
-            (pi_h, v_h), state = self.agent.policy.net(
-                agent_obs, state, context={"first": first})
+            # Need to run this with no_grad or else the gradient descent will crash during training lol
+            with th.no_grad():
+                (pi_h, v_h), state = self.agent.policy.net(
+                    agent_obs, state, context={"first": first})
 
-            pi_distribution = self.agent.policy.pi_head(pi_h)
-            v_prediction = self.agent.policy.value_head(v_h)
+                pi_distribution = self.agent.policy.pi_head(pi_h)
+                v_prediction = self.agent.policy.value_head(v_h)
 
             # print(pi_distribution)
             # print(policy.get_logprob_of_action(pi_distribution, None))
@@ -167,13 +184,52 @@ class ProximalPolicyOptimizer:
         for _ in tqdm(range(self.epochs), desc="epochs"):
 
             # Shuffle the memories
-            for obs, state, pi_h, v_h, action, action_log_prob, reward, done, value in dl:
-                # Run the model on ALL the memories in the batch
+            # Note: These are batches! Not individual samples
+            for obs, states, pi_h, v_h, actions, old_action_log_probs, rewards, dones, v_old in dl:
+                # Run the model on the batch using the latent state output
                 pi_distribution = self.agent.policy.pi_head(pi_h)
                 v_prediction = self.agent.policy.value_head(v_h)
 
-                print(pi_distribution["camera"].shape)
-                print(v_prediction.shape)
+                # Get log probs
+                action_log_probs = self.agent.policy.get_logprob_of_action(
+                    pi_distribution, actions)
+
+                entropy = self.agent.policy.pi_head.entropy(pi_distribution)
+
+                # Calculate clipped surrogate objective
+                ratios = (action_log_probs - old_action_log_probs).exp()
+                advantages = normalize(rewards - v_prediction.detach())
+                surr1 = ratios * advantages
+                surr2 = ratios.clamp(
+                    1 - self.eps_clip, 1 + self.eps_clip) * advantages
+                policy_loss = - th.min(surr1, surr2) - self.beta_s * entropy
+
+                # print(type(policy_loss))
+                # print(policy_loss.shape)
+
+                # Calculate clipped value loss
+                value_clipped = v_old + \
+                    (v_prediction - v_old).clamp(-self.value_clip, self.value_clip)
+
+                value_loss_1 = (value_clipped.squeeze() - rewards) ** 2
+                value_loss_2 = (v_prediction.squeeze() - rewards) ** 2
+
+                value_loss = th.mean(th.max(value_loss_1, value_loss_2))
+                # Calculate clipped value loss
+                # value_loss = clipped_value_loss(
+                #     v_prediction, rewards, old_values, self.value_clip)
+
+                # Update the policy network
+                self.optim_pi.zero_grad()
+                policy_loss.mean().backward()
+                self.optim_pi.step()
+
+                # Update the value network
+                self.optim_v.zero_grad()
+                value_loss.backward()
+                self.optim_v.step()
+
+                # update_network(value_loss, self.optim_v)
 
     def run_train_loop(self):
         """
@@ -194,13 +250,13 @@ if __name__ == "__main__":
     )
     ppo = ProximalPolicyOptimizer(
         "MineRLPunchCow-v0",
-        "models/foundation-model-2x.model",
-        "weights/foundation-model-2x.weights",
+        "models/foundation-model-1x.model",
+        "weights/foundation-model-1x.weights",
 
         rc=rc,
-        ppo_iterations=1,
-        episodes=1,
-        epochs=1,
+        ppo_iterations=10,
+        episodes=3,
+        epochs=10,
         minibatch_size=48
     )
 
--- src/util.py
+++ src/util.py
@@ -6,3 +6,7 @@ device = th.device("cuda:0" if th.cuda.is_available() else "cpu")
 
 
 def to_torch_tensor(t): return th.stack(t).to(device).detach()
+
+
+def normalize(t, eps=1e-5):
+    return (t - t.mean()) / (t.std() + eps)
